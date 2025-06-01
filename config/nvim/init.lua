c5bf4b27f41b94c26933d66f53c63c3c0ea161c1
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-06-01 15:37:44 -0400

    Add pylsp.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -688,7 +688,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['pylsp'].setup {
   capabilities = capabilities,
   pylsp = {
-    executable = "heloooo",
+    executable = "pylsp",
     plugins = {
       -- formatter options
       black = { enabled = true },
