6dfacdb8c4565b026d7e4dc1eadc3a8d2addd380
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2026-01-12 12:45:39 -0500

    Update ruby.sh to use asdf.

--- laptop_install/ruby.sh
+++ laptop_install/ruby.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
+brew install openssl@3 readline libyaml gmp autoconf rust
+# only for Ruby 3.0, 2.7, or older
+brew install rbenv/tap/openssl@1.1
+
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
